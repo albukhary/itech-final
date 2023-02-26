9ae48ba8a71fe05572ec744056148a9c49e73740
Lazizbek Kahramonov <lazizbekkahramonov@gmail.com>
2023-02-26 09:16:29 +0500

    Applied terraform fmt

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,4 +1,4 @@
- resource "aws_security_group" "http-sg" {
+resource "aws_security_group" "http-sg" {
   name        = "allow_http_access"
   description = "allow inbound http traffic"
   vpc_id      = var.vpc_id
@@ -33,13 +33,13 @@ data "aws_ami" "amazon_ami" {
   owners      = ["amazon"]
 }
 resource "aws_instance" "app-server1" {
-  instance_type          = "t2.micro"
-  ami                    = data.aws_ami.amazon_ami.id
-  vpc_security_group_ids = [aws_security_group.http-sg.id]
-  subnet_id              = var.subnet_id
+  instance_type               = "t2.micro"
+  ami                         = data.aws_ami.amazon_ami.id
+  vpc_security_group_ids      = [aws_security_group.http-sg.id]
+  subnet_id                   = var.subnet_id
   associate_public_ip_address = true
-    tags = {
-      Name = "app-server-1"
-    }
+  tags = {
+    Name = "app-server-1"
+  }
   user_data = file("${path.module}/user_data/user_data.tpl")
 }
--- us-east-1/vpc/main.tf
+++ us-east-1/vpc/main.tf
@@ -1,31 +1,31 @@
 terraform {
   backend "s3" {
-    bucket = "itech-final-state"
-    key = "us-east-1/vpc/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "itech-final-state"
+    key            = "us-east-1/vpc/terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "itech-final-tfstate-lock"
-    encrypt = true
+    encrypt        = true
   }
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "4.53.0"
+      source  = "hashicorp/aws"
+      version = "4.53.0"
     }
   }
   required_version = ">= 1.0.2"
 }
 
 provider "aws" {
-    region = var.aws_region
-    default_tags {
-      tags = {
-        "Environment" = "Development"
-        "Team" = "iTech-DevOps"
-        "DevelopedBy" = "Terraform"
-        "Application" = "Terraform Backend"
-        "OwnerEmail" = "lazizbekexclusive@gmail.com"
-      }
-    }    
+  region = var.aws_region
+  default_tags {
+    tags = {
+      "Environment" = "Development"
+      "Team"        = "iTech-DevOps"
+      "DevelopedBy" = "Terraform"
+      "Application" = "Terraform Backend"
+      "OwnerEmail"  = "lazizbekexclusive@gmail.com"
+    }
+  }
 }
 
 module "vpc" {
