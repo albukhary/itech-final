3495c06cfece44de43d195b2b7882ec45577cb73
Lazizbek Kahramonov <lazizbekkahramonov@gmail.com>
2023-02-26 16:27:59 +0500

    terraform fmt

--- us-east-1/compute/main.tf
+++ us-east-1/compute/main.tf
@@ -42,5 +42,5 @@ module "compute" {
   vpc_id        = data.terraform_remote_state.vpc.outputs.vpc_id
   subnet_id     = data.terraform_remote_state.vpc.outputs.subnet_id
   instance_type = var.instance_type
-  instance_num = var.instance_num
+  instance_num  = var.instance_num
 }
