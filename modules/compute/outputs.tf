6c587036ccab08b7baaf66993d8a2dbd26deeafa
Lazizbek Kahramonov <lazizbekkahramonov@gmail.com>
2023-02-26 09:23:20 +0500

    Compute module output variables fixed

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,8 +1,8 @@
-output "instance1_public_dns" {
+output "instance_public_dns" {
   value = aws_instance.app-server1.public_dns
 }
 
-output "instance1_public_ip" {
+output "instance_public_ip" {
   value = aws_instance.app-server1.public_ip
 }
 
--- us-east-1/compute/outputs.tf
+++ us-east-1/compute/outputs.tf
@@ -1,13 +1,13 @@
 output "instance_public_dns" {
-  value = module.compute.instance1_public_dns
+  value = module.compute.instance_public_dns
 }
 
 output "instance_public_ip" {
-  value = module.compute.instance1_public_ip
+  value = module.compute.instance_public_ip
 }
 
 output "instance_id" {
-  value = module.compute.instance1_id
+  value = module.compute.instance_id
 }
 
 output "sg_id" {
